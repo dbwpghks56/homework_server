6d7499c6249ab64b302eee603b5353ea26f37aa5
wpghks <wpghks56@naver.com>
2023-06-20 15:03:42 +0900

    [feature] Project Init

---
+++ src/main/kotlin/com/homework/server/homework_server/HomeworkServerApplication.kt
@@ -0,0 +1,11 @@
+package com.homework.server.homework_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HomeworkServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<HomeworkServerApplication>(*args)
+}
---
+++ src/test/kotlin/com/homework/server/homework_server/HomeworkServerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.homework.server.homework_server
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HomeworkServerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
