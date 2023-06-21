db4ce62c1b85ac6394eec8d846540545b3ce84c4
wpghks <wpghks56@naver.com>
2023-06-21 13:13:01 +0900

    [feature] swagger 에 대한 description 작성

--- src/main/kotlin/com/homework/server/homework_server/oembed/dto/response/OEmbedResponseDto.kt
+++ src/main/kotlin/com/homework/server/homework_server/oembed/dto/response/OEmbedResponseDto.kt
@@ -1,27 +1,76 @@
 package com.homework.server.homework_server.oembed.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDateTime
 import java.util.Date
 
 data class OEmbedResponseDto(
+    @Schema(
+        description = "제목"
+    )
     val title: String? = null,
+    @Schema(
+        description = "해당 링크 내용물에 대한 type"
+    )
     val type: String? = null,
+    @Schema(
+        description = "버전"
+    )
     val version: Float? = null,
+    @Schema(
+        description = "링크에 대한 정보를 제공하는 곳의 이름"
+    )
     val provider_name: String? = null,
+    @Schema(
+        description = "링크에 대한 정보를 제공하는 곳의 주소"
+    )
     val provider_url: String? = null,
+    @Schema(
+        description = "링크에 해당하는 정보를 게시한 사람"
+    )
     val author_name : String? = null,
+    @Schema(
+        description = "링크에 해당하는 정보를 게시한 페이지(계정) 주소"
+    )
     val author_url: String? = null,
     val is_plus:Int? = null,
+    @Schema(
+        description = "링크에 대한 내용을 게시할 때 쓰는 html"
+    )
     val html: String? = null,
+    @Schema(
+        description = "링크에 대한 내용을 게시할 때 쓰는 html의 넓이"
+    )
     val width: Int? = null,
+    @Schema(
+        description = "링크에 대한 내용을 게시할 때 쓰는 html의 높이"
+    )
     val height: Int? = null,
     val duration: Int? = null,
+    @Schema(
+        description = "링크에 대한 내용의 설명"
+    )
     val description: String? = null,
+    @Schema(
+        description = "링크에 대한 내용의 썸네일"
+    )
     val thumbnail_url: String? = null,
+    @Schema(
+        description = "링크에 대한 내용의 썸네일 넓이"
+    )
     val thumbnail_width: Int? = null,
+    @Schema(
+        description = "링크에 대한 내용의 썸네일 높이"
+    )
     val thumbnail_height: Int? = null,
     val thumbnail_url_with_play_button: String? = null,
+    @Schema(
+        description = "링크에 대한 내용의 업로드 일"
+    )
     val upload_date: String? = null,
+    @Schema(
+        description = "링크에 대한 내용의 id"
+    )
     val video_id: Int? = null,
     val cache_age: Long? = null,
     val url:String? = null
--- src/main/kotlin/com/homework/server/homework_server/oembed/web/OEmbedController.kt
+++ src/main/kotlin/com/homework/server/homework_server/oembed/web/OEmbedController.kt
@@ -2,17 +2,28 @@ package com.homework.server.homework_server.oembed.web
 
 import com.homework.server.homework_server.oembed.dto.response.OEmbedResponseDto
 import com.homework.server.homework_server.oembed.service.OEmbedService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/OEmbed")
+@RequestMapping("/oEmbed")
+@Tag(name = "[App] oEmbed Data", description = "oEmbed Data API")
 class OEmbedController(
     private val oEmbedService: OEmbedService
 ) {
     @GetMapping()
+    @Tag(name = "[App] oEmbed Data")
+    @Operation(
+        summary = "oEmbed 해당 데이터 조회",
+        description = "사용자에게 받은 url 데이터를 이용해서 oEmbed 데이터 조회"
+    )
+    @ResponseStatus(HttpStatus.OK)
     fun getOEmbedInfo(
         url:String
     ): ResponseEntity<OEmbedResponseDto> {
