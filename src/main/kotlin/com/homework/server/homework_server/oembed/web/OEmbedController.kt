62adc814a65310ccad5a64e7c635b6bb198d286b
wpghks <wpghks56@naver.com>
2023-06-21 17:01:40 +0900

    [feature] crossorigin으로 일단 cors 에러 해제

--- src/main/kotlin/com/homework/server/homework_server/oembed/web/OEmbedController.kt
+++ src/main/kotlin/com/homework/server/homework_server/oembed/web/OEmbedController.kt
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/oEmbed")
+@CrossOrigin
 @Tag(name = "[App] oEmbed Data", description = "oEmbed Data API")
 class OEmbedController(
     private val oEmbedService: OEmbedService
